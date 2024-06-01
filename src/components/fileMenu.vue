9dd835a41a2b456dd96c559202713083cce8a930
weifegn <1982392655@qq.com>
2024-06-01 10:08:57 +0800

    feat: add iconfont.

--- src/components/fileMenu.vue
+++ src/components/fileMenu.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="file-menu">file-menu</div>
+  <div class="file-menu">
+    <div class="file-menu-icon">
+      <i v-for="(i, d) in icons" :key="i" class="iconfont" :class="i"></i>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 定义顶部icon - 静态
+const icons = [
+  "icon-xinjianwenjian",
+  "icon-xinjianwenjianjia",
+  "icon-zidongliuchengxiafaliebiao",
+];
+</script>
 
 <style lang="less" scoped>
+@import url("../assets/font_4568361_difd97dq5u4/iconfont.css");
 .file-menu {
   width: 220px;
   border-right: solid #ccc 1px;
